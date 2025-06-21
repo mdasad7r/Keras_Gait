a171e557217ba06cca504273a5e5041dad894683
Asad <asadmohd9411@gmail.com>
2025-06-21 05:44:46 +0530

    Update cnn_encoder.py

--- model/cnn_encoder.py
+++ model/cnn_encoder.py
@@ -3,41 +3,27 @@ from tensorflow.keras import layers, models
 def build_cnn_encoder(input_shape=(64, 64, 1), feature_dim=256):
     inputs = layers.Input(shape=input_shape)
 
-    # Block 1
-    x = layers.Conv2D(32, (3, 3), padding='same')(inputs)
-    x = layers.ReLU()(x)
+    x = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
     x = layers.BatchNormalization()(x)
     x = layers.MaxPooling2D((2, 2))(x)
 
-    # Block 2 (2 layers)
-    x = layers.Conv2D(64, (3, 3), padding='same')(x)
-    x = layers.ReLU()(x)
-    x = layers.BatchNormalization()(x)
-
-    x = layers.Conv2D(64, (3, 3), padding='same')(x)
-    x = layers.ReLU()(x)
+    # Block 2
+    x = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.MaxPooling2D((2, 2))(x)
 
-    # Block 3 (2 layers)
-    x = layers.Conv2D(128, (3, 3), padding='same')(x)
-    x = layers.ReLU()(x)
-    x = layers.BatchNormalization()(x)
-
-    x = layers.Conv2D(128, (3, 3), padding='same')(x)
-    x = layers.ReLU()(x)
+    # Block 3
+    x = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.MaxPooling2D((2, 2))(x)
 
-    # Block 4
-    x = layers.Conv2D(256, (3, 3), padding='same')(x)
-    x = layers.ReLU()(x)
+    # Block 4 (new)
+    x = layers.Conv2D(256, (3, 3), activation='relu', padding='same')(x)
     x = layers.BatchNormalization()(x)
     x = layers.GlobalAveragePooling2D()(x)
 
     # Final dense projection
-    x = layers.Dense(feature_dim)(x)
-    x = layers.Activation('sigmoid')(x)  # Final sigmoid (as requested)
+    x = layers.Dense(feature_dim, activation='relu')(x)
 
     model = models.Model(inputs, x, name='cnn_encoder')
     return model
